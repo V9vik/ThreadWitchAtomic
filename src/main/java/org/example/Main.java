3ff0831d3dae4a00e8ef685847a26a63a638bc7d
V9va <myzikov03@gmail.com>
2024-10-31 22:48:33 +0300

    Inital commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,63 @@
+package org.example;
+
+import javax.sound.midi.Soundbank;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Main {
+    public static AtomicInteger lengthWord3 =new AtomicInteger(0);
+    public static AtomicInteger lengthWord4 = new AtomicInteger(0);
+    public static AtomicInteger lengthWord5 = new AtomicInteger(0);
+    public static void main(String[] args) throws InterruptedException {
+        Random random = new Random();
+        String[] texts = new String[100_000];
+        for (int i = 0; i < texts.length; i++) {
+            texts[i] = generateText("abc", 3 + random.nextInt(3));
+        }
+        new Thread(()-> {
+            for (int i = 0; i <100_000 ; i++) {
+                if(texts[i].length() == 3 && isPalindrome(texts[i])){
+                    lengthWord3.incrementAndGet();
+                }
+            }
+        }).start();
+        new Thread(()-> {
+            for (int i = 0; i < 100_000; i++) {
+                if(texts[i].length() == 4 && isPalindrome(texts[i])){
+                    lengthWord4.incrementAndGet();
+                }
+            }
+        }).start();
+        new Thread(() -> {
+            for (int i = 0; i < 100_000; i++) {
+                if(texts[i].length() ==5 && isPalindrome(texts[i])){
+                    lengthWord5.incrementAndGet();
+                }
+            }
+        }).start();
+        System.out.println("Красивых слов с длиной 3:" + lengthWord3 + " шт");
+        System.out.println("Красивых слов с длиной 4:" + lengthWord4+ " шт");
+        System.out.println("Красивых слов с длиной 5:" + lengthWord5 + " шт");
+    }
+    public static boolean isPalindrome(String word) {
+        int left = 0;
+        int right = word.length() - 1;
+
+        while (left < right) {
+            if (Character.toLowerCase(word.charAt(left++)) != Character.toLowerCase(word.charAt(right--))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static String generateText(String letters, int length) {
+        Random random = new Random();
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            text.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return text.toString();
+    }
+}
